2a36a0cbdd459c3d46062dd3465a98df01ae6d57
Luis <luigichevez@gmail.com>
2017-10-04 11:55:51 -0600

    Master

--- src/main/java/com/sv/udb/modelos/Entregas.java
+++ src/main/java/com/sv/udb/modelos/Entregas.java
@@ -11,6 +11,8 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -37,6 +39,7 @@ public class Entregas implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "codi_entr")
     private Integer codiEntr;
